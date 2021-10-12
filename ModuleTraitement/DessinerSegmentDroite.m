73adb7e032c2b9f9b09a68b4b9d6ef344b3bb120
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-10-12 14:03:23 -0400

    Update DessinerSegmentDroite.m

--- ModuleTraitement/DessinerSegmentDroite.m
+++ ModuleTraitement/DessinerSegmentDroite.m
@@ -5,8 +5,17 @@ function DessinerSegmentDroite(x1,y1,x2,y2)
 %Cette fonction permet de tracer une ligne droite entre deux points donnés
 
 pente = PenteDroite(x1,y1,x2,y2);
-ordOrigine = OrdonneDroite(x1,y1,pente);
-for ii = x1:0.0001:x2
+ordOrigine = OrdonneeDroite(x1,y1,pente);
+
+if(x1>x2)
+    pDepartX=x2;
+    pFinX=x1;
+else
+    pDepartX=x1;
+    pFinX=x2;
+end
+
+for ii = pDepartX:0.0001:pFinX
     AfficherDonnee(ii,ii*pente+ordOrigine);
 end
 end
