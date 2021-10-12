c4e1991ff8426432e6e639d9b5d9e44711ee9474
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-10-12 14:16:56 -0400

    Update TraiterSignal.m

--- ModuleTraitement/TraiterSignal.m
+++ ModuleTraitement/TraiterSignal.m
@@ -17,20 +17,26 @@ global tempsECG  ;
 %On intialise une valeur pour dénombrer le nombre de mesures prises
 nbMesures=0;
 
+prochainTemps=0;
+prochainVoltage=0;
 %On passe à travers toutes les valeurs de x qui doivent être affichées
 for ii = 1:numel(tempsECG)
     %On vérifie que le temps de la valeur à afficher est inférieur  au
     %temps maximal récupéré
     while(tempsECG(ii)<= xMax)
+        if(modeTracage == 1)
+            AfficherDonnee(prochainTemps,prochainVoltage);
+        end
+        if(modeTracage==2&&nbMesures>=2)
+            DessinerSegmentDroite(temps,voltage,prochainTemps,prochainVoltage);
+        end
+        temps=prochainTemps;
+        voltage=prochainVoltage;
         %On obtient les valeurs de la prochaine mesure
         [prochainTemps,prochainVoltage] = ObtenirProchaineMesure();
         %On itère nbMesures pour compter le nombre de mesures faites
         nbMesures = nbMesures + 1;
         %Si le mode de traçage choisi est 1, on affiche les données sur le
         %graphe
-        if(modeTracage == 1)
-            AfficherDonnee(prochainTemps,prochainVoltage);
-        end
     end
-end
 end
